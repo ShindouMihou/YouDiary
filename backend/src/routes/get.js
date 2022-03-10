b2624d08a948d1b7db1e2827f5d7e74361927c48
Shindou <kokocrunchsh@gmail.com>
2022-03-10 20:16:09 +0800

    Fix invalid names returned from backend

--- backend/src/routes/get.js
+++ backend/src/routes/get.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 router.get('/', (req, res) => res.send({
     name: config.BUCKET_NAME,
-    containers: fs.readdirSync(config.BUCKET_PATH).filter(file => file.endsWith('.json')).map(file => file.replace('.json', ''))
+    containers: fs.readdirSync(config.BUCKET_PATH).filter(file => file.endsWith('.json')).map(file => file.replace('.json', '')).map(file => Buffer.from(file, 'base64url').toString('ascii'))
 }));
 
 module.exports = router;
